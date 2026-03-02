b2ce374b8074f513ad9334be1e13b7c81e511a08
moisesph <mpleytez@byupathway.edu>
2026-03-02 09:21:59 -0600

    daf

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static double PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string sNumber = Console.ReadLine();
+            double number = int.Parse(sNumber);
+            return number;
+        }
+        static double SquareNumber(double number)
+        {
+            double squared = number * number;
+            return squared;
+        }
+
+        static void DisplayResult(string name, double squared)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squared}");
+        }
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        double number = PromptUserNumber();
+        double squared = SquareNumber(number);
+        DisplayResult(name, squared);
     }
 }
