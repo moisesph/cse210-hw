1a4c04c3221b8ad373a8ddb064b81b70a5785b83
moisesph <mpleytez@byupathway.edu>
2026-03-01 22:02:20 -0600

    fda

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is the grade? ");
+        string sGrade = Console.ReadLine();
+
+        int grade = int.Parse(sGrade);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+
+        }
+
+        else
+        {
+            letter = "F";
+
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You have not passed");
+        }
+
     }
 }
